d491c615bb813ad260ecc3fb48711ff7a21a7541
Gigan20 <mosi.1400.email@gmail.com>
2026-02-21 21:40:58 +0330

    Update login.php

--- login.php
+++ login.php
@@ -37,184 +37,182 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     <title>login</title>
 </head>
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=SN+Pro:ital,wght@0,200..900;1,200..900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=SN+Pro:ital,wght@0,200..900;1,200..900&display=swap");
+::selection {
+  background: #00f1;
+}
+* {
+  font-family: "SN Pro";
+  transition: 0.3s;
+}
 
-    ::selection {
-        background: #00f1;
-    }
+body {
+  background: url("https://s34.picofile.com/file/8490306050/bg.jpg"),#eff;
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  min-height: 100vh;
+  background-position: center center;
+}
+.flex{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.loaded {
+  animation: bganim forwards 1s 1s;
+}
+@keyframes bganim {
+  to {
+    backdrop-filter: blur(10px) brightness(1.1);
+  }
+}
+form {
+  width: 100%;
+}
+.box {
+  width: 100%;
+  transform: scale(1.2);
+  opacity: 0;
+  filter: blur(15px);
+}
+.loaded .box {
+  animation: anim forwards 1s 1.2s;
+}
+@keyframes anim {
+  to {
+    filter: blur(0);
+    opacity: 1;
+    transform: scale(1);
+  }
+}
 
-    * {
-        font-family: "SN Pro";
-        transition: 0.3s;
-    }
+h1 {
+  padding: 0 1rem;
+  text-align: center;
+  margin: 0;
+  font-size: 30px;
+  margin-bottom: 0.5rem;
+}
 
-    body {
-        background: url("https://images.unsplash.com/photo-1701198067981-bb371b7621a7?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzE2MjI4NDd8&ixlib=rb-4.1.0&q=85"), #eff;
-        background-size: cover;
-        background-repeat: no-repeat;
-        padding: 0;
-        margin: 0;
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        min-height: 100vh;
-        animation: bganim forwards 1s 1s;
-        background-position: center center;
-    }
+input {
+  background: #fffc;
+  border-radius: 5px;
+  padding: 0.4rem 1rem;
+  box-shadow: #0001 2px 2px 2px;
+  outline: none;
+  border: none;
+}
 
-    @keyframes bganim {
-        to {
-            backdrop-filter: blur(10px);
-        }
-    }
-    form{
-        width:100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
-    .box {
-        width:100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        transform: scale(1.2);
-        opacity: 0;
-        filter: blur(15px);
-        animation: anim forwards 1s 1.2s;
-    }
+button {
+  border: none;
+  background: #fffa;
+  padding: 0.4rem 1rem;
+  border-radius: 50px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.btndiv {
+  margin-top: 1rem;
+}
+.loader {
+  border-radius: 30px;
+  padding: 0;
+  width: 11px;
+  display: inline-block;
+  margin-left: -15px;
+  border: 2px #0004 solid;
+  border-right-color: #000;
+  height: 11px;
+  opacity: 0;
+  animation: loadanim 0.5s forwards, rotate 1s linear infinite;
+}
+@keyframes loadanim {
+  to {
+    opacity: 1;
+    margin-left: 0.3rem;
+  }
+}
+@keyframes rotate {
+  to {
+    transform: rotate(1turn);
+  }
+}
+button:hover,
+a:hover {
+  transform: scale(1.05);
+}
 
-    @keyframes anim {
-        to {
-            filter: blur(0);
-            opacity: 1;
-            transform: scale(1);
-        }
-    }
+button:active,
+a:active {
+  transform: scale(0.95);
+}
 
-    h1 {
-        padding: 0 1rem;
-        text-align: center;
-        margin: 0;
-        font-size: 30px;
-        margin-bottom: 0.5rem;
-    }
+legend {
+  margin-top: 0.4rem;
+}
 
-    input {
-        background: #fffc;
-        border-radius: 5px;
-        padding: .4rem 1rem;
-        box-shadow: #0001 2px 2px 2px;
-        outline: none;
-        border: none;
-    }
+p {
+  margin: 0;
+  margin-bottom: -31px;
+  display: inline-block;
+  font-size: 14px;
+  text-align: center;
+  background: #f00a;
+  color: #fff;
+  border-radius: 100px;
+  padding: 0.4rem 1rem;
+  filter: blur(15px);
+  opacity: 0;
+  transform: scale(0.8);
+}
+.loaded p {
+  animation: erranim forwards 5s 3s;
+}
+a {
+  font-size: 14px;
+  color: #000;
+  padding: 0 0 0 1rem;
+  display: inline-block;
+  text-decoration: none;
+}
 
-    button {
-        border: none;
-        background: #fffa;
-        padding: .4rem 1rem;
-        border-radius: 50px;
-        display: inline-flex;
-        cursor: pointer;
-    }
-    .btndiv{
-        margin-top:1rem;
-    }
-    .loader{
-        border-radius:30px;
-        padding:0;
-        width: 13px;
-        display:inline-block;
-        margin-left:-17px;
-        border:2px #0004 solid;
-        border-right-color:#000;
-        height: 13px;
-        opacity:0;
-        animation:loadanim  .5s forwards,rotate 1s linear infinite;
-    }
-    @keyframes loadanim{
-        to{opacity:1;margin-left:.3rem;}
-    }
-        @keyframes rotate{
-        to{transform: rotate(1turn);}
-    }
-    button:hover,
-    a:hover {
-        transform: scale(1.05);
-    }
+@keyframes erranim {
+  10%,
+  80% {
+    margin: 0 0 1rem 0;
+    filter: blur(0);
+    opacity: 1;
+    transform: scale(1);
+  }
+  90% {
+    margin: 0;
+    margin-bottom: -31px;
+    filter: blur(15px);
+    opacity: 0;
+    transform: scale(0.8);
+  }
+}
 
-    button:active,
-    a:active {
-        transform: scale(.95);
-    }
+.loadnotice {
+  width: 300px;
+  transition: 1s;
+  text-align: center;
+  top: calc(50% - 15px);
+  left: calc(50% - 150px - 1rem);
+  position: fixed;
+}
+.loaded .loadnotice {
+  transform: scale(1.2);
+  opacity: 0;
+  filter: blur(15px);
+}
 
-    legend {
-        margin-top: .4rem;
-    }
-
-    p {
-        margin:0;
-        margin-bottom:-31px;
-        display: inline-block;
-        font-size: 14px;
-        text-align: center;
-        background: #f00a;
-        color: #fff;
-        border-radius: 100px;
-        padding: .4rem 1rem;
-        filter: blur(15px);
-        opacity: 0;
-        transform: scale(.8);
-        animation: erranim forwards 5s 1s;
-    }
-
-    a {
-        font-size: 14px;
-        color: #000;
-        padding: 0 0 0 1rem;
-        display: inline-block;
-        text-decoration: none;
-    }
-
-    @keyframes erranim {
-        10%,80% {
-            margin: 0 0 1rem 0;
-            filter: blur(0);
-            opacity: 1;
-            transform: scale(1);
-        }
-        90% {
-            margin:0;
-            margin-bottom:-31px;
-            filter: blur(15px);
-            opacity: 0;
-            transform: scale(.8);
-        }
-    }
-
-    .log {
-        color: #fff;
-        background: #0009;
-        margin: .5rem;
-        padding: 1rem;
-        border-radius: 5px;
-        width: 300px;
-        font-family: monospace;
-        color:#6f6;
-    }
-
-    .log h1 {
-        font-size: 13px;
-        text-align: left;
-        width: 100%;
-        margin-left: -1rem;
-        margin:none;
-        padding: 0 1rem;
-    }
 
     <?php if (isset($_POST["username"]) && isset($_POST["password"])): ?>body,
     .box {
@@ -234,11 +232,12 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     <?php endif; ?>
 </style>
 
-<body>
+<body class="flex">
+    <h1 class="loadnotice">please wait</h1>
     <div class="box">
         <?php if (!empty($result)): ?><p><?php echo $result; ?></p><?php endif; ?>
         <h1>welcome to todolist.com</h1>
-        <form method="post" action="">
+        <form method="post" action="" class="flex">
             <div><legend>username</legend>
             <input type="username" name="username"></div>
             <div><legend>password</legend>
@@ -253,6 +252,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
         const button = document.querySelector("button");
         button.innerHTML = "login<div class=\"loader\"></div>";
     }
+    function load(){const button = document.querySelector("button");button.innerHTML = "login<div class=\"loader\"></div>";}
+window.addEventListener('load', () => {document.body.classList.add('loaded');});
     </script>
-
 </html>
